7c4a5f1e234af37d725567f53646c929e6e676c4
frankfang <frankfang1990@gmail.com>
2022-10-26 22:55:34 +0800

    完成第一个页面的样式

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -21,21 +21,21 @@ export const WelcomeLayout: React.FC = () => {
     config: { duration: 300 }
   })
   return (
-    <div>
-      <header>
-        <img src={logo} />
-        <h1>山竹记账</h1>
+    <div className="bg-#5f34bf" h-screen flex flex-col items-stretch pb-16px>
+      <header shrink-0 text-center pt-64px>
+        <img src={logo} w-64px />
+        <h1 text="#D4D4EE" text-32px>山竹记账</h1>
       </header>
-      <main>
+      <main shrink-1 grow-1 bg-white m-16px rounded-8px flex justify-center items-center>
         {transitions((style, pathname) =>
           <animated.div key={pathname} style={style}>
             {map.current[pathname]}
           </animated.div>
         )}
       </main>
-      <footer>
-        <Link to={linkMap[location.pathname]}>下一页</Link>
-        <Link to="/welcome/xxx">跳过</Link>
+      <footer shrink-0 text-center text-24px text-white grid grid-cols-3 grid-rows-1>
+        <Link style={{ gridArea: '1 / 2 / 2 / 3' }} to={linkMap[location.pathname]}>下一页</Link>
+        <Link style={{ gridArea: '1 / 3 / 2 / 4' }} to="/welcome/xxx">跳过</Link>
       </footer>
     </div>
   )
--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,9 +1,9 @@
 import p1 from '../assets/images/welcome1.svg'
 export const Welcome1: React.FC = () => {
   return (
-    <div>
+    <div text-center>
       <img src={p1} />
-      <h2>
+      <h2 text-32px mt-48px >
         会挣钱 <br/>
         还要会省钱
       </h2>
