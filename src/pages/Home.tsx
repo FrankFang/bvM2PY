1206793cffd4f4e261fc3a5de151fb110b607803
frankfang <frankfang1990@gmail.com>
2022-11-09 22:48:05 +0800

    演示如何发送关联请求

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,16 @@
+import axios from 'axios'
+import useSWR from 'swr'
 import p from '../assets/images/pig.svg'
 import add from '../assets/icons/add.svg'
 export const Home: React.FC = () => {
+  const { data: meData, error: meError } = useSWR('/api/v1/me', (path) => {
+    return axios.get(`http://121.196.236.94:8080${path}`)
+  })
+  const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, (path) => {
+    return axios.get(`http://121.196.236.94:8080${path}`)
+  })
+  console.log(meData, meError, itemsData, itemsError)
+
   return <div>
     <div flex justify-center items-center>
       <img mt-20vh mb-20vh width="128" height="130" src={p} />
