c067b4286b5a8229a7b98b45e7dbf92b26a077aa
frankfang <frankfang1990@gmail.com>
2022-11-17 22:31:36 +0800

    完成 ItemsSummary 组件

--- src/components/TimeRangePicker.tsx
+++ src/components/TimeRangePicker.tsx
@@ -4,13 +4,13 @@ interface Props {
   selected: TimeRange
   onSelected: (selected: TimeRange) => void
 }
+const timeRanges: { key: TimeRange; text: string }[] = [
+  { key: 'thisMonth', text: '本月' },
+  { key: 'lastMonth', text: '上月' },
+  { key: 'thisYear', text: '今年' },
+  { key: 'custom', text: '自定义时间' },
+]
 export const TimeRangePicker: React.FC<Props> = ({ selected, onSelected }) => {
-  const timeRanges: { key: TimeRange; text: string }[] = [
-    { key: 'thisMonth', text: '本月' },
-    { key: 'lastMonth', text: '上月' },
-    { key: 'thisYear', text: '今年' },
-    { key: 'custom', text: '自定义时间' },
-  ]
   return (
     <ol flex text-white children-px-24px children-py-12px cursor-pointer>
       {timeRanges.map(tr => <li key={tr.key} className={tr.key === selected ? s.selected : ''}
--- src/pages/ItemsPage/ItemsSummary.tsx
+++ src/pages/ItemsPage/ItemsSummary.tsx
@@ -1,3 +1,19 @@
 export const ItemsSummary: React.FC = () => {
-  return <div>ItemsSummary </div>
+  return (
+    <ol bg="#252A43" flex justify-between items-center m-16px rounded-8px py-12px px-16px
+      children-px-24px text-center>
+      <li text="#FE7275">
+        <div>收入</div>
+        <div>1000</div>
+      </li>
+      <li text="#53A867">
+        <div>支出</div>
+        <div>1000</div>
+      </li>
+      <li text-white>
+        <div>净收入</div>
+        <div>1000</div>
+      </li>
+    </ol>
+  )
 }
