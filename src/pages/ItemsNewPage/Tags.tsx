a3e7b5fb186e0cf99a2ff7287d66307507751b4d
frankfang <frankfang1990@gmail.com>
2022-12-09 22:51:51 +0800

    完善 Tags 组件的样式细节

--- src/pages/ItemsNewPage/Tags.tsx
+++ src/pages/ItemsNewPage/Tags.tsx
@@ -1,15 +1,27 @@
+import { Icon } from '../../components/Icon'
+
 type Props = {
   kind: Item['kind']
 }
 export const Tags: React.FC<Props> = (props) => {
   const { kind } = props
-  const tags = Array.from({ length: 99 })
+  const tags = Array.from({ length: 21 })
   return (
     <div>
       <ol grid grid-cols="[repeat(auto-fit,48px)]" justify-center gap-x-32px
-      gap-y-16px py-16px px-8px>
+        gap-y-16px py-16px px-8px>
+        <li>
+          <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
+            flex justify-center items-center text-24px text="#8F4CD7"
+            ><Icon name="add" /></span>
+        </li>
         {tags.map(tag =>
-          <li w-48px h-48px b-1 b-red flex justify-center items-center>😶</li>
+          <li w-48px flex justify-center items-center
+            flex-col gap-y-8px>
+            <span block w-48px h-48px rounded="24px" bg="#EFEFEF"
+              flex justify-center items-center text-24px b-1 b="#8F4CD7">😶</span>
+            <span text-12px text="#666">打车</span>
+          </li>
         )}
       </ol>
     </div>
