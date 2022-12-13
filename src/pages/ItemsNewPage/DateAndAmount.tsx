765c2f17079704a204492a41bd5ded4bb0377e1f
frankfang <frankfang1990@gmail.com>
2022-12-13 22:35:10 +0800

    完成数字面板的样式

--- src/pages/ItemsNewPage/DateAndAmount.tsx
+++ src/pages/ItemsNewPage/DateAndAmount.tsx
@@ -1,26 +1,36 @@
 import { useState } from 'react'
+import { Icon } from '../../components/Icon'
 
 type Props = {
   className?: string
 }
 export const DateAndAmount: React.FC<Props> = (props) => {
   const { className } = props
-  const [x, setX] = useState('')
   return (
-    <div b-1 b-blue className={className}>
-      <input value={x} onChange={e => setX(e.target.value)} />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-      DateAndAmount <br />
-
+    <div className={className}>
+      <div flex p-t-15px p-b-16px px-16px border-t-1px border-t="#ddd" gap-x-8px items-center>
+        <Icon name="calendar" className="w-24px h-24px grow-0 shrink-0" />
+        <span grow-0 shrink-0 text-12px color="#999">2001-02-03</span>
+        <code grow-1 shrink-1 text-right color="#53A867">123456789.01</code>
+      </div>
+      <div py-1px grid
+        grid-cols="[repeat(4,1fr)]" grid-rows="[repeat(4,56px)]" bg="#ddd" gap-1px
+        children-b-none
+        children-bg-white>
+        <button row-start-1 col-start-1 row-end-2 col-end-2>1</button>
+        <button row-start-1 col-start-2 row-end-2 col-end-3>2</button>
+        <button row-start-1 col-start-3 row-end-2 col-end-4>3</button>
+        <button row-start-2 col-start-1 row-end-3 col-end-2>4</button>
+        <button row-start-2 col-start-2 row-end-3 col-end-3>5</button>
+        <button row-start-2 col-start-3 row-end-3 col-end-4>6</button>
+        <button row-start-3 col-start-1 row-end-4 col-end-2>7</button>
+        <button row-start-3 col-start-2 row-end-4 col-end-3>8</button>
+        <button row-start-3 col-start-3 row-end-4 col-end-4>9</button>
+        <button row-start-4 col-start-1 row-end-5 col-end-3>0</button>
+        <button row-start-4 col-start-3 row-end-5 col-end-4>.</button>
+        <button row-start-1 col-start-4 row-end-3 col-end-5>清空</button>
+        <button row-start-3 col-start-4 row-end-5 col-end-5>提交</button>
+      </div>
     </div>
   )
 }
