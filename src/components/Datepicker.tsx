fa8518c860ee2e5f7118dd49de379b2c6c7a055b
frankfang <frankfang1990@gmail.com>
2022-12-30 22:16:53 +0800

    提供不加 new 就可以使用的 time 函数

--- src/components/Datepicker.tsx
+++ src/components/Datepicker.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { time } from '../lib/time'
 
 type Props = {
   start?: Date
--- src/lib/time.ts
+++ src/lib/time.ts
@@ -11,6 +11,11 @@ type Unit =
   | 'year' | 'years' | 'month' | 'months' | 'day' | 'days'
   | 'hour' | 'hours' | 'minute' | 'minutes' | 'second' | 'seconds'
   | 'ms'
+
+export const time = (p?: number | string | Date) => {
+  return new Time(p)
+}
+
 export class Time {
   date: Date
   constructor(p?: number | string | Date) {
