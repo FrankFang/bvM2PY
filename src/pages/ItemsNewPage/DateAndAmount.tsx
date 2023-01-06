951a634bfb47a026f96de213fd8b8d5cc2d1ada3
frankfang <frankfang1990@gmail.com>
2023-01-06 21:55:52 +0800

    用户可以输入金额，最多两位小数，最长16个字符

--- src/pages/ItemsNewPage/DateAndAmount.tsx
+++ src/pages/ItemsNewPage/DateAndAmount.tsx
@@ -9,10 +9,35 @@ type Props = {
 }
 export const DateAndAmount: React.FC<Props> = (props) => {
   const [date, setDate] = useState(new Date())
+  const [output, _setOutput] = useState('0')
+  // 拦截器
+  const setOutput = (str: string) => {
+    const dotIndex = str.indexOf('.')
+    if (dotIndex >= 0 && str.length - dotIndex > 3) { return }
+    if (str.length > 16) { return }
+    _setOutput(str)
+  }
   const { className } = props
   const { toggle, popup, hide } = usePopup(false, <Datepicker
     onConfirm={d => { setDate(d); hide() }}
     onCancel={() => hide()} />)
+  const append = (char: string) => {
+    switch (char) {
+      case '0':
+        if (output !== '0') { setOutput(output + char) }
+        break
+      case '.':
+        if (!output.includes('.')) { setOutput(output + char) }
+        break
+      default:
+        if (output === '0') { setOutput(char) }
+        else { setOutput(output + char) }
+        break
+    }
+  }
+  const clear = () => {
+    setOutput('0')
+  }
   return (
     <>
       {popup}
@@ -22,25 +47,23 @@ export const DateAndAmount: React.FC<Props> = (props) => {
             <Icon name="calendar" className="w-24px h-24px grow-0 shrink-0" />
             <span grow-0 shrink-0 color="#999">{time(date).format()}</span>
           </span>
-          <code grow-1 shrink-1 text-right color="#53A867">123456789.01</code>
+          <code grow-1 shrink-1 text-right color="#53A867">{output}</code>
         </div>
-        <div py-1px grid
-          grid-cols="[repeat(4,1fr)]" grid-rows="[repeat(4,56px)]" bg="#ddd" gap-1px
-          children-b-none
-          children-bg-white>
-          <button row-start-1 col-start-1 row-end-2 col-end-2>1</button>
-          <button row-start-1 col-start-2 row-end-2 col-end-3>2</button>
-          <button row-start-1 col-start-3 row-end-2 col-end-4>3</button>
-          <button row-start-2 col-start-1 row-end-3 col-end-2>4</button>
-          <button row-start-2 col-start-2 row-end-3 col-end-3>5</button>
-          <button row-start-2 col-start-3 row-end-3 col-end-4>6</button>
-          <button row-start-3 col-start-1 row-end-4 col-end-2>7</button>
-          <button row-start-3 col-start-2 row-end-4 col-end-3>8</button>
-          <button row-start-3 col-start-3 row-end-4 col-end-4>9</button>
-          <button row-start-4 col-start-1 row-end-5 col-end-3>0</button>
-          <button row-start-4 col-start-3 row-end-5 col-end-4>.</button>
-          <button row-start-1 col-start-4 row-end-3 col-end-5>清空</button>
-          <button row-start-3 col-start-4 row-end-5 col-end-5>提交</button>
+        <div py-1px grid children-b-none children-bg-white
+          grid-cols="[repeat(4,1fr)]" grid-rows="[repeat(4,56px)]" bg="#ddd" gap-1px >
+          <button row-start-1 col-start-1 row-end-2 col-end-2 onClick={() => append('1')}>1</button>
+          <button row-start-1 col-start-2 row-end-2 col-end-3 onClick={() => append('2')}>2</button>
+          <button row-start-1 col-start-3 row-end-2 col-end-4 onClick={() => append('3')}>3</button>
+          <button row-start-2 col-start-1 row-end-3 col-end-2 onClick={() => append('4')}>4</button>
+          <button row-start-2 col-start-2 row-end-3 col-end-3 onClick={() => append('5')}>5</button>
+          <button row-start-2 col-start-3 row-end-3 col-end-4 onClick={() => append('6')}>6</button>
+          <button row-start-3 col-start-1 row-end-4 col-end-2 onClick={() => append('7')}>7</button>
+          <button row-start-3 col-start-2 row-end-4 col-end-3 onClick={() => append('8')}>8</button>
+          <button row-start-3 col-start-3 row-end-4 col-end-4 onClick={() => append('9')}>9</button>
+          <button row-start-4 col-start-1 row-end-5 col-end-3 onClick={() => append('0')}>0</button>
+          <button row-start-4 col-start-3 row-end-5 col-end-4 onClick={() => append('.')}>.</button>
+          <button row-start-1 col-start-4 row-end-3 col-end-5 onClick={clear}>清空</button>
+          <button row-start-3 col-start-4 row-end-5 col-end-5 onClick={() => { }}>提交</button>
         </div>
       </div>
     </>
