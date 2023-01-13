8ce089c02bde204a4a4c815c2933cf7c12ce0937
frankfang <frankfang1990@gmail.com>
2023-01-13 22:22:34 +0800

    将 error 重命名为 newError，这样就不会和 store 里的 error 重名了。

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -13,14 +13,14 @@ export const SignInPage: React.FC = () => {
   const nav = useNavigate()
   const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
-    const error = validate(data, [
+    const newError = validate(data, [
       { key: 'email', type: 'required', message: '请输入邮箱地址' },
       { key: 'email', type: 'pattern', regex: /^.+@.+$/, message: '邮箱地址格式不正确' },
       { key: 'code', type: 'required', message: '请输入验证码' },
       { key: 'code', type: 'length', min: 6, max: 6, message: '验证码必须是6个字符' },
     ])
-    setError(error)
-    if (!hasError(error)) {
+    setError(newError)
+    if (!hasError(newError)) {
       await ajax.post('/api/v1/session', data)
       // TODO
       // 保存 JWT 作为登录凭证
