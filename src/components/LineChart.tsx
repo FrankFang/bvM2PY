a4bca57d6d513935abace2c0db6fd9eda1052eb2
frankfang <frankfang1990@gmail.com>
2023-01-18 23:03:45 +0800

    基本晚上折线图的样式

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -13,18 +13,47 @@ export const LineChart: React.FC<Props> = (props) => {
   useEffect(() => {
     if (!div.current) { return }
     const myChart = echarts.init(div.current)
-    const option = {
+    const option: echarts.EChartsOption = {
+      tooltip: {
+        trigger: 'axis',
+        show: true,
+        formatter: ([{ axisValue, data }]: any) => {
+          const parts = axisValue.split('-')
+          const label = `${parts[0]}年${parts[1]}月${parts[2]}日`
+          const value = data === null ? '无数据' : `${data}元`
+          return `${label}<br/><div style="text-align: right;">${value}</div>`
+        }
+      },
+      grid: {
+        left: 16,
+        top: 8,
+        bottom: 24,
+        right: 16
+      },
       xAxis: {
         type: 'category',
         data: xItems,
+        axisLabel: {
+          formatter: (label: string) => label.slice(label.indexOf('-') + 1)
+        },
       },
       yAxis: {
-        type: 'value'
+        type: 'value',
+        axisLabel: {
+          show: false
+        },
       },
       series: [
         {
           data: yItems,
-          type: 'line'
+          type: 'line',
+          itemStyle: {
+          },
+          emphasis: {
+            itemStyle: {
+              color: 'green'
+            }
+          }
         }
       ]
     }
--- src/pages/StatisticsPage.tsx
+++ src/pages/StatisticsPage.tsx
@@ -12,8 +12,35 @@ export const StatisticsPage: React.FC = () => {
   const items = [
     { date: '2000-01-01', value: 15000 },
     { date: '2000-01-02', value: 25000 },
+    { date: '2000-01-03', value: 25000 },
+    { date: '2000-01-04', value: 35000 },
+    { date: '2000-01-05', value: 35000 },
+    { date: '2000-01-06', value: 45000 },
+    { date: '2000-01-07', value: 45000 },
+    { date: '2000-01-08', value: 55000 },
+    { date: '2000-01-09', value: 55000 },
+    { date: '2000-01-10', value: 65000 },
+    { date: '2000-01-11', value: 65000 },
+    { date: '2000-01-12', value: 75000 },
+    { date: '2000-01-13', value: 75000 },
+    { date: '2000-01-14', value: 85000 },
+    { date: '2000-01-15', value: 85000 },
+    { date: '2000-01-16', value: 95000 },
+    { date: '2000-01-17', value: 95000 },
+    { date: '2000-01-18', value: 105000 },
+    { date: '2000-01-19', value: 105000 },
+    { date: '2000-01-20', value: 115000 },
+    { date: '2000-01-21', value: 115000 },
+    { date: '2000-01-22', value: 125000 },
+    { date: '2000-01-23', value: 125000 },
+    { date: '2000-01-24', value: 135000 },
+    { date: '2000-01-25', value: 135000 },
+    { date: '2000-01-26', value: 145000 },
+    { date: '2000-01-27', value: 145000 },
+    { date: '2000-01-28', value: 155000 },
+    { date: '2000-01-29', value: 155000 },
     { date: '2000-01-31', value: 10000 },
-  ].map(item => ({ x: item.date, y: item.value }))
+  ].map(item => ({ x: item.date, y: item.value / 100 }))
   return (
     <div>
       <Gradient>
@@ -22,7 +49,7 @@ export const StatisticsPage: React.FC = () => {
         } />
       </Gradient>
       <TimeRangePicker selected={timeRange} onSelect={setTimeRange} />
-      <LineChart className="h-400px" items={items} />
+      <LineChart className="h-120px" items={items} />
     </div>
   )
 }
