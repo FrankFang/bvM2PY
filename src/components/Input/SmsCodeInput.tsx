47d4310a2ac1c2606b5c70af4973894b19d669e6
frankfang <frankfang1990@gmail.com>
2023-02-03 21:58:36 +0800

    倒计时的 bug 解决（但还是有可能间隔超过 1 秒）

--- src/components/Input/SmsCodeInput.tsx
+++ src/components/Input/SmsCodeInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 type Props = {
   value?: string
@@ -6,34 +6,47 @@ type Props = {
   onChange?: (value: string) => void
   request?: () => Promise<unknown>
 }
+const maxCount = 60
 export const SmsCodeInput: React.FC<Props> = (props) => {
   const { value, placeholder, onChange, request } = props
-  const [started, setStarted] = useState(false)
-  const [count, setCount] = useState(5)
+  const [started, setStarted] = useState<Date>()
+  const [count, setCount] = useState(maxCount)
+  const timer = useRef<number>()
   const onClick = async () => {
     if (!request) { return }
     await request()
-    setStarted(true)
+    setStarted(new Date())
   }
   useEffect(() => {
-    if (!started) { return }
-    if (count === -1) {
-      setStarted(false)
-      setCount(5)
-      return
+    if (started) {
+      timer.current = window.setInterval(() => {
+        const t = new Date()
+        const seconds = Math.round((t.getTime() - started.getTime()) / 1000)
+        if (maxCount - seconds < 0) {
+          setStarted(undefined)
+        }
+        setCount(maxCount - seconds)
+      }, 1000)
+    } else {
+      if (timer.current) {
+        window.clearInterval(timer.current)
+        timer.current = undefined
+      }
     }
-    const timer = setTimeout(() => {
-      setCount(count - 1)
-    }, 1000)
-    return () => clearTimeout(timer)
-  }, [started, count])
+    return () => {
+      if (timer.current) {
+        window.clearInterval(timer.current)
+        timer.current = undefined
+      }
+    }
+  }, [started])
 
   return (
     <div flex gap-x-16px>
       <input shrink-1 j-input-text type="text" placeholder={placeholder} max-w="[calc(40%-8px)]"
         value={value} onChange={e => onChange?.(e.target.value)} />
       {started
-        ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn>{count}</button>
+        ? <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn disabled text-gray>{count}</button>
         : <button type="button" max-w="[calc(60%-8px)]" shrink-0 j-btn onClick={onClick}>
           发送验证码
         </button>
