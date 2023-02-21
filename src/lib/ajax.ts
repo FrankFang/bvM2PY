73283038968c59ff71876a6bbe4629c7021c78dc
FrankFang <frankfang1990@gmail.com>
2023-02-21 23:48:09 +0800

    Ajax 统一错误处理

--- src/lib/ajax.ts
+++ src/lib/ajax.ts
@@ -1,5 +1,6 @@
-import type { AxiosRequestConfig } from 'axios'
+import type { AxiosError, AxiosRequestConfig } from 'axios'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { useLoadingStore } from '../stores/useLoadingStore'
 
 // 静态配置项直接用 defaults 配置
@@ -17,17 +18,44 @@ axios.interceptors.request.use((config) => {
 
 type Options = {
   showLoading?: boolean
+  handleError?: boolean
 }
 export const useAjax = (options?: Options) => {
+  const table: Record<string, undefined | (() => void)> = {
+    401: () => {
+      nav('/sign_in')
+    },
+    402: () => {
+      window.alert('请付费后观看')
+    },
+    403: () => {
+      window.alert('没有权限')
+    },
+    unknown: () => {
+      window.alert('未知错误')
+    }
+  }
   const showLoading = options?.showLoading || false
+  const handleError = options?.handleError ?? true
   const { setVisible } = useLoadingStore()
+  const nav = useNavigate()
+  const onError = (error: AxiosError) => {
+    if (error.response) {
+      if (handleError) {
+        const { status } = error.response
+        const fn = table[status] || table.unknown
+        fn?.()
+      }
+    }
+    throw error
+  }
   const ajax = {
     get: <T>(path: string, config?: AxiosRequestConfig<any>) => {
-      return axios.get<T>(path, config)
+      return axios.get<T>(path, config).catch(onError)
     },
     post: <T>(path: string, data: JSONValue) => {
       if (showLoading) { setVisible(true) }
-      return axios.post<T>(path, data).finally(() => {
+      return axios.post<T>(path, data).catch(onError).finally(() => {
         if (showLoading) { setVisible(false) }
       })
     },
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,19 +11,10 @@ interface Props {
 }
 export const Home: React.FC<Props> = (props) => {
   useTitle(props.title)
-  const nav = useNavigate()
-  const onHttpError = (error: AxiosError) => {
-    if (error.response) {
-      if (error.response.status === 401) {
-        nav('/sign_in')
-      }
-    }
-    throw error
-  }
-  const { get } = useAjax()
+  const { get } = useAjax({ showLoading: true, handleError: false })
   const { data: meData, error: meError } = useSWR('/api/v1/me', async path => {
     // 如果返回 403 就让用户先登录
-    const response = await get<Resource<User>>(path).catch(onHttpError)
+    const response = await get<Resource<User>>(path)
     return response.data.resource
   })
   const { data: itemsData, error: itemsError } = useSWR(meData ? '/api/v1/items' : null, async path =>
