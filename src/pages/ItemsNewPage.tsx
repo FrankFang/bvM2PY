4d513406f9eb00dc7088a3a100ab1a8f5b040fbe
FrankFang <frankfang1990@gmail.com>
2023-02-28 16:13:42 +0800

    使得新建 item 表单可以提交

--- src/pages/ItemsNewPage.tsx
+++ src/pages/ItemsNewPage.tsx
@@ -1,4 +1,4 @@
-import type { ReactNode } from 'react'
+import type { FormEventHandler, ReactNode } from 'react'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { Tabs } from '../components/Tabs'
@@ -11,19 +11,21 @@ import { Tags } from './ItemsNewPage/Tags'
 
 export const ItemsNewPage: React.FC = () => {
   const { data, error, setData, setError } = useCreateItemStore()
-  const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[]
-    = [
-      { // JetBrains Mono
-        key: 'expenses', text: '支出', element:
-          <Tags kind="expenses" value={data.tag_ids} onChange={(ids) => setData({ tag_ids: ids })} />
-      },
-      {
-        key: 'income', text: '收入', element:
-          <Tags kind="income" value={data.tag_ids} onChange={(ids) => setData({ tag_ids: ids })} />
-      }
-    ]
+  const tabItems: { key: Item['kind']; text: string; element?: ReactNode }[] = [
+    { // JetBrains Mono
+      key: 'expenses', text: '支出', element:
+        <Tags kind="expenses" value={data.tag_ids} onChange={(ids) => setData({ tag_ids: ids })} />
+    },
+    {
+      key: 'income', text: '收入', element:
+        <Tags kind="income" value={data.tag_ids} onChange={(ids) => setData({ tag_ids: ids })} />
+    }
+  ]
+  const onSubmit = () => {
+    console.log('你要提交是吧')
+  }
   return (
-    <div className={s.wrapper} h-screen flex flex-col>
+    <div className={s.wrapper} h-screen flex flex-col onSubmit={onSubmit}>
       <Gradient className="grow-0 shrink-0">
         <TopNav title="记一笔" icon={<Icon name="back" />} />
       </Gradient>
@@ -34,7 +36,7 @@ export const ItemsNewPage: React.FC = () => {
       <div text-28px>{JSON.stringify(data)}</div>
       <ItemAmount className="grow-0 shrink-0" itemDate={
         <ItemDate value={data.happen_at} onChange={(happen_at) => setData({ happen_at })} />
-      } value={data.amount} onChange={amount => setData({ amount })} />
+      } value={data.amount} onChange={amount => setData({ amount })} onSubmit={onSubmit} />
     </div>
   )
 }
--- src/pages/ItemsNewPage/ItemAmount.tsx
+++ src/pages/ItemsNewPage/ItemAmount.tsx
@@ -6,9 +6,10 @@ type Props = {
   itemDate: ReactNode
   value?: number
   onChange?: (amount: number) => void
+  onSubmit?: () => void
 }
 export const ItemAmount: React.FC<Props> = (props) => {
-  const { value, onChange } = props
+  const { value, onChange, onSubmit } = props
   const [output, _setOutput] = useState(() => value?.toString() ?? '0')
   // 拦截器
   const setOutput = (str: string) => {
@@ -45,20 +46,20 @@ export const ItemAmount: React.FC<Props> = (props) => {
         </div>
         <div py-1px grid children-b-none children-bg-white
           grid-cols="[repeat(4,1fr)]" grid-rows="[repeat(4,56px)]" bg="#ddd" gap-1px >
-          <button row-start-1 col-start-1 row-end-2 col-end-2 onClick={() => append('1')}>1</button>
-          <button row-start-1 col-start-2 row-end-2 col-end-3 onClick={() => append('2')}>2</button>
-          <button row-start-1 col-start-3 row-end-2 col-end-4 onClick={() => append('3')}>3</button>
-          <button row-start-2 col-start-1 row-end-3 col-end-2 onClick={() => append('4')}>4</button>
-          <button row-start-2 col-start-2 row-end-3 col-end-3 onClick={() => append('5')}>5</button>
-          <button row-start-2 col-start-3 row-end-3 col-end-4 onClick={() => append('6')}>6</button>
-          <button row-start-3 col-start-1 row-end-4 col-end-2 onClick={() => append('7')}>7</button>
-          <button row-start-3 col-start-2 row-end-4 col-end-3 onClick={() => append('8')}>8</button>
-          <button row-start-3 col-start-3 row-end-4 col-end-4 onClick={() => append('9')}>9</button>
-          <button row-start-4 col-start-1 row-end-5 col-end-3 onClick={() => append('0')}>0</button>
-          <button row-start-4 col-start-3 row-end-5 col-end-4 onClick={() => append('.')}>.</button>
-          <button row-start-1 col-start-4 row-end-3 col-end-5 onClick={clear}>清空</button>
-          <button row-start-3 col-start-4 row-end-5 col-end-5
-            bg="#5C33BE" text-white onClick={() => { }}>提交</button>
+          <button type="button" row-start-1 col-start-1 row-end-2 col-end-2 onClick={() => append('1')}>1</button>
+          <button type="button" row-start-1 col-start-2 row-end-2 col-end-3 onClick={() => append('2')}>2</button>
+          <button type="button" row-start-1 col-start-3 row-end-2 col-end-4 onClick={() => append('3')}>3</button>
+          <button type="button" row-start-2 col-start-1 row-end-3 col-end-2 onClick={() => append('4')}>4</button>
+          <button type="button" row-start-2 col-start-2 row-end-3 col-end-3 onClick={() => append('5')}>5</button>
+          <button type="button" row-start-2 col-start-3 row-end-3 col-end-4 onClick={() => append('6')}>6</button>
+          <button type="button" row-start-3 col-start-1 row-end-4 col-end-2 onClick={() => append('7')}>7</button>
+          <button type="button" row-start-3 col-start-2 row-end-4 col-end-3 onClick={() => append('8')}>8</button>
+          <button type="button" row-start-3 col-start-3 row-end-4 col-end-4 onClick={() => append('9')}>9</button>
+          <button type="button" row-start-4 col-start-1 row-end-5 col-end-3 onClick={() => append('0')}>0</button>
+          <button type="button" row-start-4 col-start-3 row-end-5 col-end-4 onClick={() => append('.')}>.</button>
+          <button type="button" row-start-1 col-start-4 row-end-3 col-end-5 onClick={clear}>清空</button>
+          <button onClick={onSubmit} type="submit" row-start-3 col-start-4 row-end-5 col-end-5
+            bg="#5C33BE" text-white >提交</button>
         </div>
       </div>
     </>
