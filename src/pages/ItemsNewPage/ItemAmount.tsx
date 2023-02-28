b9fa2ceb5641e13f8dcff1565d65a1689962cc3b
FrankFang <frankfang1990@gmail.com>
2023-02-28 16:54:28 +0800

    amount 的单位为分，UI需要展示为元

--- src/pages/ItemsNewPage/ItemAmount.tsx
+++ src/pages/ItemsNewPage/ItemAmount.tsx
@@ -10,14 +10,14 @@ type Props = {
 }
 export const ItemAmount: React.FC<Props> = (props) => {
   const { value, onChange, onSubmit } = props
-  const [output, _setOutput] = useState(() => value?.toString() ?? '0')
+  const [output, _setOutput] = useState(() => value ? (value / 100).toString() : '0')
   // 拦截器
   const setOutput = (str: string) => {
     const dotIndex = str.indexOf('.')
     if (dotIndex >= 0 && str.length - dotIndex > 3) { return }
     if (str.length > 16) { return }
     _setOutput(str)
-    onChange?.(parseFloat(str))
+    onChange?.(parseFloat(str) * 100)
   }
   const { className } = props
   const append = (char: string) => {
