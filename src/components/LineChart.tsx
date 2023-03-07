b50d4920ba6907914acf093d16605eb3bf50a70b
FrankFang <frankfang1990@gmail.com>
2023-03-07 23:45:58 +0800

    折线图会在数据更新时重新渲染

--- src/components/LineChart.tsx
+++ src/components/LineChart.tsx
@@ -11,10 +11,11 @@ export const LineChart: React.FC<Props> = (props) => {
   const xItems = items?.map(item => item.x)
   const yItems = items?.map(item => item.y)
   const initialized = useRef(false)
+  const myChart = useRef<echarts.ECharts>()
   useEffect(() => {
     if (!div.current) { return }
     if (initialized.current) { return }
-    const myChart = echarts.init(div.current)
+    myChart.current = echarts.init(div.current)
     initialized.current = true
     const option: echarts.EChartsOption = {
       tooltip: {
@@ -60,8 +61,15 @@ export const LineChart: React.FC<Props> = (props) => {
         }
       ]
     }
-    myChart.setOption(option)
+    myChart.current.setOption(option)
   }, [])
+  useEffect(() => {
+    const option: echarts.EChartsOption = {
+      xAxis: { data: xItems, },
+      series: [{ data: yItems, }]
+    }
+    myChart.current?.setOption(option)
+  }, [items])
   return (
     <div ref={div} className={className}></div>
   )
