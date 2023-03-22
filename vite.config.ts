be0f74b59450e749901a3f67451f7b2f91d18703
FrankFang <frankfang1990@gmail.com>
2023-03-23 00:52:55 +0800

    手动分块 manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,20 @@ import { svgsprites } from './vite_plugins/svgsprites'
 export default defineConfig((env) => {
   const { command } = env
   return {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id: any) {
+            if (id.includes('echarts')) {
+              return 'echarts'
+            }
+            if (id.includes('node_modules')) {
+              return 'vendor'
+            }
+          }
+        }
+      }
+    },
     server: {
       proxy: {
         '/api/': {
